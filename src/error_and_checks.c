70996dd3e3ab2c613b087dd98f92d46fff16a094
Aapo Kivioja <akivioja@f0r6s8.codam.nl>
2023-05-19 15:31:37 +0200

    error check for largest int added

--- src/error_and_checks.c
+++ src/error_and_checks.c
@@ -6,7 +6,7 @@
 /*   By: akivioja <akivioja@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/22 14:48:06 by akivioja      #+#    #+#                 */
-/*   Updated: 2023/05/18 16:32:58 by akivioja      ########   odam.nl         */
+/*   Updated: 2023/05/19 15:31:08 by akivioja      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,6 +74,27 @@ int	is_sorted(char **argv, int argc)
 	return (-1);
 }
 
+int	large_int_check(int argc, char **argv)
+{
+	int		i;
+	long	value;
+
+	i = 1;
+	while (i < argc)
+	{
+		value = 0;
+		while (*argv[i] >= '0' && *argv[i] <= '9')
+		{
+			value = value * 10 + (*argv[i] - '0');
+			argv[i]++;
+		}
+		if (value > 2147483647)
+			return (-1);
+		i++;
+	}
+	return (0);
+}
+
 int	error_check(char **argv, int argc)
 {
 	int	count;
@@ -82,6 +103,8 @@ int	error_check(char **argv, int argc)
 	if (has_duplicates(argv, argc) == -1)
 		return (-1);
 	if (non_numeric(argv, argc) == -1)
+		return (-1);
+	if (large_int_check(argc, argv) == -1)
 		return (-1);
 	return (0);
 }
